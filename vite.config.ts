6371e2b72ecf2895547a71b926392aa68759efdb
magang <nupc_magang@outlook.com>
2026-06-30 12:45:13 +0700

    Fix: update deploy.yml and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/arief.prasetyo911/",
+  base: "/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
